a01ed160d125aabab9927e957d818f3e1ff4bac8
ramya.reddy161 <ramya.reddy161@gmail.com>
2018-09-29 22:12:17 +0530

    mistakes

--- frontend/src/Authentication/Login.js
+++ frontend/src/Authentication/Login.js
@@ -9,7 +9,7 @@ class Login extends Component{
     cookies = new Cookies();
     state = {
         auth_url : 'https://ramyareddy-colloquium.herokuapp.com/api-basictoken-auth/',
-        jwt_url : 'https://ramyareddy16-colloquium.herokuapp.com/api-jwttoken-auth/',
+        jwt_url : 'https://ramyareddy-colloquium.herokuapp.com/api-jwttoken-auth/',
         // auth_url : 'http://127.0.0.1:8000/api-basictoken-auth/',
         // jwt_url : 'http://127.0.0.1:8000/api-jwttoken-auth/',
         buttonName : 'Login',
